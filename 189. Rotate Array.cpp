e2dc5c2e4cbc9770194583412aacec43d792cf63
Naveen0978 <119524198+Naveen0978@users.noreply.github.com>
2023-03-05 12:39:53 +0530

    Create 189. Rotate Array.cpp
    Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    void Reverse(vector<int>& nums,int start,int end)
+    {
+        while(start <= end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n==0)
+            return;
+        k = k%n;
+        Reverse(nums,0,n-k-1);
+        Reverse(nums,n-k,n-1);
+        Reverse(nums,0,n-1);
+    }
+};
